92166a38879b5bd4318eb807587d4188eb292f37
TANIT0 <114485425+TANIT0@users.noreply.github.com>
2023-03-20 11:14:46 +0100

    Create area_del_circulo.js

---
+++ area_del_circulo.js
@@ -0,0 +1,6 @@
+function area_circulo(radio){
+    let total = Math.PI * radio * radio;
+    return total;
+}
+total=area_circulo(23);
+console.log("el area de un circulo con radio 23 es " + Math.PI * radio * radio)
