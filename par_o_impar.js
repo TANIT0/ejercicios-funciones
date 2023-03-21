0d678c9611aa56f40dd79815facd3ac647fce761
TANIT0 <114485425+TANIT0@users.noreply.github.com>
2023-03-21 13:23:58 +0100

    Create par_o_impar.js

---
+++ par_o_impar.js
@@ -0,0 +1,10 @@
+function par_o_imp(numero){
+    if(numero % 2==0){
+        return "es par";
+    }else{
+        return "es impar";
+    }
+}
+
+console.log("23-->"+ par_o_imp(23));
+console.log("5-->"+ par_o_imp(5));
